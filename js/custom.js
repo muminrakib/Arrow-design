d1262dceda4b9635e41f1e1cee516bf2b5948679
Abdul Mumin <mumin.rakib@yahoo.com>
2018-02-22 01:56:40 +0600

    arrow design 22-02-18

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,19 @@
 // My custom js
-$('body').scrollspy({ target: '#navbar-example' })
+// SmoothScroll
+var scroll = new SmoothScroll('a[href*="#"]');
+// scrollspy
+
+
+//button
+	// $("button").not(".navbar-toggler").click(function() {
+	// 	alert("You have clicked the button")/* Act on the event */
+	// });
+//button
+//addclass removeclass practice
+$("#top").click(function(){
+    $(this).toggleClass("btn-warning");
+});
+$(".nav-item").click(function(){
+    $(this).addClass("active");
+    $(".nav-item").not(this).removeClass("active");
+});
