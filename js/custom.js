f0f56ff9bc57bd02b1889d1b72fff920b1a42441
Abdul Mumin <mumin.rakib@yahoo.com>
2018-02-23 02:12:31 +0600

    23-02-18
    -add wow
    -validate html

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,17 @@
 // My custom js
-// SmoothScroll
+// ==SmoothScroll==
 var scroll = new SmoothScroll('a[href*="#"]');
 // scrollspy
 
 
-//button
-	// $("button").not(".navbar-toggler").click(function() {
-	// 	alert("You have clicked the button")/* Act on the event */
-	// });
-//button
-//addclass removeclass practice
+//==addclass removeclass practice==
 $("#top").click(function(){
     $(this).toggleClass("btn-warning");
 });
 $(".nav-item").click(function(){
-    $(this).addClass("active bg-info");
-    $(".nav-item").not(this).removeClass("active bg-info");
+    $(this).addClass("bg-info");
+    $(".nav-item").not(this).removeClass("bg-info");
 });
+
+//==wow==
+new WOW().init();
