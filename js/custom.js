af5dc83d99263a291db19edc8b7bde04e00e6dcd
Abdul Mumin <mumin.rakib@yahoo.com>
2018-03-08 02:25:29 +0600

    08-03-18

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,14 @@
-// My custom js
+// My custom js 
+// navbar click fixed
+$(".nav-item").on("click",function(){
+  $(".navbar-collapse").removeClass('show');
+});
 // ==SmoothScroll==
 var scroll = new SmoothScroll('a[href*="#"]');
 // scrollspy
 $('body').scrollspy({ target: '#navbarSupportedContent' })
 
 //==addclass removeclass practice==
-$("#top").click(function(){
-    $(this).toggleClass("btn-warning");
-});
 $(".nav-item").click(function(){
     $(this).addClass("bg-info");
     $(".nav-item").not(this).removeClass("bg-info");
@@ -16,5 +17,44 @@ $(".nav-item").click(function(){
 //==wow==
 new WOW().init();
 
-// site preloader -- also uncomment the div in the header and the css style for #preloader
-// site preloader -- also uncomment the div in the header and the css style for #preloader
+// custom dataspy
+
+$("#home").on("mouseenter", function(){
+	$("a[href='#home']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(1)").removeClass('bg-info');
+});
+$("#about").on("mouseenter", function(){
+	$("a[href='#about']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(2)").removeClass('bg-info');
+});
+$("#services").on("mouseenter", function(){
+	$("a[href='#services']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(3)").removeClass('bg-info');
+});
+$("#design").on("mouseenter", function(){
+	$("a[href='#design']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(4)").removeClass('bg-info');
+});
+$("#team").on("mouseenter", function(){
+	$("a[href='#team']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(5)").removeClass('bg-info');
+});
+$("#portfolio").on("mouseenter", function(){
+	$("a[href='#portfolio']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(6)").removeClass('bg-info');
+});
+$("#package").on("mouseenter", function(){
+	$("a[href='#package']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(7)").removeClass('bg-info');
+});
+$("#address").on("mouseenter", function(){
+	$("a[href='#address']").parent().addClass('bg-info');
+	$(".nav-item").not(".nav-item:nth-child(8)").removeClass('bg-info');
+});
+
+
+// navbar click fixed [news page]
+
+$(".sidebar-nav").on("click",function(){
+  $("#wrapper").removeClass('toggled');
+});
---
+++ readme.txt
@@ -0,0 +1,21 @@
+Arrow Design is a HTML5 & CSS3 responsive template created for IT service companies but also can be used for generalised website. 
+
+It is a fully responsive, feature rich and beautifully designed to host a website or create online identity.
+
+It supports bootstrap framework and font awesome icon so easy to customise and develop your own styles.
+
+
+Your main code reside in Template directory. You have to upload every file from main directory to your server for properly work.
+For info please refer documentation.
+
+
+Thank you for purchasing Klinik Template. If you have any questions that are beyond the scope of this help file and documentation, 
+please feel free to contact us at support.pepdev.com or mail us at abdul@naturalsearch.com.
+
+
+
+Key Features:
+-created with mdbootstrap, font awesome v5
+-home page, blog page, contact page included
+-pluging used: scrollspy, wow, smoothscroll
+-fully responsive
